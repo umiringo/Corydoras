1c23739cdcdc165ec9b123ac5764b4853cd5b175
umiringo <umiringo42@gmail.com>
2017-09-27 19:28:36 +0800

    add basic

---
+++ Corydoras/Assets/Scripts/FSM/FiniteState.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum StateTransition
+{
+	NullTransition = 0,
+	Start = 1,
+	Quit = 2,
+}
+
+public enum StateID
+{
+	NullStateID = 0,
+	MainMenu = 1,
+	GameScene = 2,
+}
+
+public abstract class FiniteState
+{
+	protected Dictionary<StateTransition, StateID> _stateTransitionMap = new Dictionary<StateTransition, StateID>();
+	protected StateID _stateID;
+	public StateID ID { get { return _stateID; }}
+
+	public void AddTransition(StateTransition trans, StateID id)
+	{
+		if( trans == StateTransition.NullTransition ) {
+			return;
+		}
+		if( id == StateID.NullStateID ) {
+			return;
+		}
+
+		if( _stateTransitionMap.ContainsKey(trans) ) {
+			return;
+		}
+		_stateTransitionMap.Add(trans, id);
+	}
+
+	public void DeleteTransition(StateTransition trans)
+	{
+		if( trans == StateTransition.NullTransition )
+		{
+			return;
+		}
+
+		if(_stateTransitionMap.ContainsKey(trans))
+		{
+			_stateTransitionMap.Remove(trans);
+			return;
+		}
+	}
+
+	public StateID GetTargetState(StateTransition trans)
+	{
+		if(_stateTransitionMap.ContainsKey(trans))
+		{
+			return _stateTransitionMap[trans];
+		}
+		return StateID.NullStateID;
+	}
+
+	public virtual void DoBeforeEnter() {}
+	public virtual void DoBeforeQuit() {}
+
+	// public abstract void Reason(GameObject go);
+	// public abstract void Act(GameObject go);
+}
---
+++ Corydoras/Assets/Scripts/FSM/FiniteStateMachine.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FiniteStateMachine
+{
+	private List<FiniteState> _stateList;
+
+	private StateID _currentStateID;
+	public StateID CurrentStateID { get { return _currentStateID; } }
+
+	private FiniteState _currentState;
+	public FiniteState CurretnState { get { return _currentState; } }
+
+	public FiniteStateMachine()
+	{
+		_stateList = new List<FiniteState>();
+		_currentStateID = StateID.NullStateID;
+	}
+
+	public void AddFiniteState(FiniteState fs)
+	{
+		if( fs == null ) {
+			return;
+		}
+
+		if( _stateList.Count == 0 ) {
+			_stateList.Add(fs);
+			_currentState = fs;
+			_currentStateID = fs.ID;
+			return;
+		}
+
+		foreach( FiniteState fState in _stateList ) {
+			if( fState.ID == fs.ID ) {
+				return;
+			}
+		}
+		_stateList.Add(fs);
+	}
+
+	public void DeleteFiniteState(StateID id)
+	{
+		if( id == StateID.NullStateID ) {
+			return;
+		}
+
+		foreach( FiniteState fs in _stateList ) {
+			if( fs.ID == id ) {
+				_stateList.Remove(fs);
+				return;
+			}
+		}
+	}
+
+	public void PerformTransition(StateTransition trans)
+	{
+		if( trans == StateTransition.NullTransition ) {
+			return;
+		}
+
+		StateID id = _currentState.GetTargetState(trans);
+		if( id == StateID.NullStateID ) {
+			return;
+		}
+
+		_currentStateID = id;
+		foreach (FiniteState fs in _stateList) {
+			if( fs.ID == _currentStateID ) {
+				_currentState.DoBeforeQuit();
+				_currentState = fs;
+				_currentState.DoBeforeEnter();
+				break;
+			}
+		}
+	}
+}
