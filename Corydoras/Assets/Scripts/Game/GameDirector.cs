a1534db1ffcb5da4b68b59b34fb173520d572b33
umiringo <umiringo42@gmail.com>
2017-09-30 11:38:31 +0800

    add GameDelegate.cs

--- Corydoras/Assets/Scripts/Game/GameDirector.cs
+++ Corydoras/Assets/Scripts/Game/GameDirector.cs
@@ -4,13 +4,31 @@ using UnityEngine;
 
 public class GameDirector : MonoBehaviour {
 
+    private int score;
+    private int highScore;
+
 	// Use this for initialization
 	void Start () {
-		
+		GamePlayMgr.Instance.Init();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    private void StartMain()
+    {
+
+    }
+
+    private void StartGame()
+    {
+
+    }
+
+    private void GameFailed()
+    {
+
+    }
 }
