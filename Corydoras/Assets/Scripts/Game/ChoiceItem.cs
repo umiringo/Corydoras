010c062b701335c10c402653086949c92741e1de
umiringo <umiringo42@gmail.com>
2017-09-30 11:30:33 +0800

    add GamePanel

--- Corydoras/Assets/Scripts/Game/ChoiceItem.cs
+++ Corydoras/Assets/Scripts/Game/ChoiceItem.cs
@@ -1,16 +1,26 @@
-﻿using System.Collections;
+﻿﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
 
 public class ChoiceItem : MonoBehaviour {
+    public int index;
+    public Transform riddleTrans;
 
-	// Use this for initialization
-	void Start () {
-		
+    // Use this for initialization
+    void Start () {
+        transform.DOScale(new Vector3(0.9f, 0.9f, 1.0f), 1.0f).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.Linear);
+       // transform.DORotate(new Vector3(0.0f, 0.0f, -360.0f), 10.0f, RotateMode.FastBeyond360).SetEase(Ease.Linear).SetLoops(-1, LoopType.Restart);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        transform.RotateAround(riddleTrans.position, new Vector3(0.0f, 0.0f, 1.0f), 20 * Time.deltaTime);
 	}
+
+    public void OnClick()
+    {
+        
+    }
 }
---
+++ Corydoras/Assets/Scripts/Game/Question.cs
@@ -0,0 +1,26 @@
+﻿﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using GlobalDefines;
+using DG.Tweening;
+
+public class Question : MonoBehaviour {
+    public int index;
+    public Text riddleText;
+    // Use this for initialization
+    void Start () {
+        transform.DOScale(new Vector3(0.9f, 0.9f, 1.0f), 1.5f).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.Linear);
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        
+    }
+
+    public void ShowRiddle(int idx, KanaType type)
+    {
+        index = idx;
+        riddleText.text = GamePlayMgr.Instance.GetKana(idx, type);
+    }
+}
